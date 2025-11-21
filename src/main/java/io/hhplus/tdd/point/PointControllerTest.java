f9c61402c777054078150d558a177a3a598dd830
yujung0 <137154455+yujung0@users.noreply.github.com>
2025-11-21 14:35:30 +0900

    202511211434_가짜정답구현하기

---
+++ src/main/java/io/hhplus/tdd/point/PointControllerTest.java
@@ -0,0 +1,44 @@
+package io.hhplus.tdd.point;
+
+import io.hhplus.tdd.database.PointHistoryTable;
+import io.hhplus.tdd.database.UserPointTable;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class PointControllerTest {
+
+private UserPointTable userPointTable;
+private PointHistoryTable pointHistoryTable;
+private PointController pointController;
+
+
+
+@BeforeEach
+    void setUp() {
+    userPointTable = new UserPointTable();
+    pointHistoryTable = new PointHistoryTable();
+    pointController = new PointController();
+}
+
+@Test
+@DisplayName("신규 유저 포인트 조회 시 0원을 반환한다.")
+    void NewUserCheck0won () {
+    try {
+        // given
+        long userId = 1L; // 임의로 지정한 테스트 값
+
+        // when
+        UserPoint result = pointController.point(userId); // Id로 포인트 조회
+
+        // then
+        assert result.id() == userId;   // 결과의 id와 point 비교
+        assert result.point() == 0L;// 결과의 id와 point 비교
+    } catch (AssertionError e) {
+        log.info(e.getMessage());
+    }
+}
+}
